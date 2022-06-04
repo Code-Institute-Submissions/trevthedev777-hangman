a4600341417b58c33fa0007d2dcbe20a3a69166a
trevthedev777 <bloodsportrecords@gmail.com>
2022-06-04 13:32:56 +0000

    Create Hangman Animation

--- run.py
+++ run.py
@@ -8,4 +8,89 @@ def get_word():
     word = random.choice(word_list)
     return word.upper()
 
+# def play(word):
+#     """Function to guess the word"""
+#     wordCompletion = "_" * len(word)
+#     guess = False
+#     guessedLetters = []
+#     guessedWords = []
+#     tries = 6
+#     print("Get ready to save someone's life!")
+    
 
+def hangman():
+    stages = [
+        # final state: head, torso, both arms, and both legs
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+                # head, torso, both arms, and one leg
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     /
+                   -
+                """,
+                # head, torso, and both arms
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |
+                   -
+                """,
+                # head, torso, and one arm
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |
+                   -
+                """,
+                # head and torso
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |
+                   -
+                """,
+                # head
+                """
+                   --------
+                   |      |
+                   |      O
+                   |
+                   |
+                   |
+                   -
+                """,
+                # initial empty state
+                """
+                   --------
+                   |      |
+                   |
+                   |
+                   |
+                   |
+                   -
+                """
+    ]
+    return stages
+
+    
